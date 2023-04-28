16e50b8a0364d98f3f8fcb717cd6ecb6484c7eba
ccv5ccv5 <406877924@qq.com>
2023-04-28 17:21:05 +0800

    Create marryDay.py
    条件过滤

---
+++ marryDay.py
@@ -0,0 +1,47 @@
+from lunar_python import Lunar, Solar
+from lunar_python.util import HolidayUtil
+from datetime import datetime
+import functools
+
+dateStart = Lunar.fromYmd(2023, 8, 17)
+dateEnd = Lunar.fromYmd(2024, 1, 1)
+
+def IsMarryYi(date):
+	return '嫁娶' in date.getDayYi()
+
+def IsMarryJi(date):
+	return '嫁娶' in date.getDayJi()
+
+def isWork(date):
+	work = True
+	solar = date.getSolar()
+	holiday = HolidayUtil.getHoliday(solar.getYear(), solar.getMonth(), solar.getDay())
+	if holiday is None:
+		week = solar.getWeek()
+		if 0 == week or 6 == week:
+			work = False
+	else:
+		work = holiday.isWork()
+	return work
+
+def eqDate(date1, date2):
+       return date1.toString() == date2.toString();
+
+dateCur = dateStart;
+dates = [dateCur];
+while not eqDate(dateCur,dateEnd):
+	dateCur = dateCur.next(1)
+	dates.append(dateCur)
+
+filters = [
+	IsMarryYi, 
+	lambda x: not isWork(x)
+	]
+filterFunc = functools.reduce(lambda f, g: lambda x: f(x) and g(x), filters)
+
+filtered_list = list(filter(filterFunc, dates))
+
+for date in filtered_list:
+	# print(date.getSolar().toFullString())
+	print(date.getSolar().toYmd())
+	
